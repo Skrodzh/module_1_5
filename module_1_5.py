553805287aec97ede9898719b569db73d4edc764
Skrodzh <zenit.nadir@mail.ru>
2024-08-10 20:50:19 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,17 @@
+immutable_var = (1, 2, 3, 'a', 'ab', 'abc')
+print(type( immutable_var))
+print(immutable_var)
+mutable_list = ([21, 63], 'apple', 'a')
+print(type(mutable_list))
+print(mutable_list)
+mutable_list[0][0] = 4
+print(mutable_list)
+
+
+
+
+
+
+
+
+
