c2202c9359530dfb7ba5250a442af1be950548bc
Skrodzh <zenit.nadir@mail.ru>
2024-08-14 21:09:11 +0300

    Add files via upload

--- module_1_5.py
+++ module_1_5.py
@@ -1,11 +1,14 @@
 immutable_var = (1, 2, 3, 'a', 'ab', 'abc')
 print(type( immutable_var))
 print(immutable_var)
-mutable_list = ([21, 63], 'apple', 'a')
+#immutable_var[0] = 8 # кортеж не потдерживает обращение по элементам
+print(immutable_var)
+mutable_list = [21, 63, 'apple', 'a']
 print(type(mutable_list))
 print(mutable_list)
-mutable_list[0][0] = 4
+mutable_list[0] = 4
 print(mutable_list)
+
 
 
 
